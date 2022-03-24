2c09fed54f0b9a9cb718f7b327237428af0bb64d
Danny Warren <danny@theforbiddenarts.com>
2022-03-24 17:22:44 -0400

    submit code

--- HomeworkDoLoop/Program.cs
+++ HomeworkDoLoop/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string name;
+
+do
+{
+    Console.Write("Please enter your first name, type 'EXIT' to leave: ");
+    name = Console.ReadLine();
+
+    if (name.ToLower() == "tim")
+    {
+        Console.WriteLine("Welcome professor!");
+    }
+    else
+    {
+        Console.WriteLine("Welcome student.");
+    }
+
+} while (name.ToLower() != "exit");
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 HomeworkDoLoopApp
+
+
+Create a Console Application that asks the user for thier name.
+Welcome me (Tim) as professor or anyone else as student.
+Do this until the user types "exit".
